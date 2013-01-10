6278308a339446b956be409284a36f95afa4d529
wimtv <wimtv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-10 16:53:08 +0000

    fixed live
    git-svn-id: https://plugins.svn.wordpress.org/wimvideopro/trunk@650890 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions.php
+++ functions.php
@@ -417,12 +417,14 @@ if (isset($_POST["wimtvpro_live"])) {
     $credential = get_option("wp_userWimtv") . ":" . get_option("wp_passWimtv");
     $url_live = get_option("wp_basePathWimtv") . "liveStream/" . $userpeer . "/" . $userpeer . "/hosts";
     if ($function=="modify")  $url_live .= "/" . $_GET['id'];
+	
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url_live);
     curl_setopt($ch, CURLOPT_USERPWD, $credential);
     curl_setopt($ch, CURLOPT_POST, TRUE);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
     $response = curl_exec($ch);
     curl_close($ch);
     if ($response!=""){
