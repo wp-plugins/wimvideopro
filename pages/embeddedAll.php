bd474dea80381c38cd85c77f3b2242434036ef37
wimtv <wimtv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-25 15:09:38 +0000

    New versione 2.0.4
    git-svn-id: https://plugins.svn.wordpress.org/wimvideopro/tags/2.0.4@658723 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/embeddedAll.php
@@ -0,0 +1,56 @@
+<?php
+  global $user;
+  include("../../../../wp-blog-header.php");
+  $table_name = $wpdb->prefix . 'wimtvpro_video';
+  $contentItem = $_GET['c'];
+
+  if (strlen($contentItem)>0) {
+
+    $arrayPlay = $wpdb->get_results("SELECT * FROM {$table_name} WHERE contentidentifier='" . $contentItem . "'");
+
+    if (get_option('wp_nameSkin')!="") {
+     $uploads_info = wp_upload_dir();
+        $directory =  $uploads_info["baseurl"] .  "/skinWim";
+
+      $skin = "&skin=" . $directory  . "/" . get_option('wp_nameSkin') . ".zip";
+    }
+    else
+      $skin = "";
+
+    
+
+	echo "<div style='text-align:center;height:" . $height . "px;width:" . $width . "px;'>";
+	echo "<div id='container'></div>";
+	
+	$dimensions = "width: '" . get_option("wp_widthPreview") . "', height: '" . get_option("wp_heightPreview") . "',";
+	
+	$dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf"; 
+	echo "<script type='text/javascript'>jwplayer('container').setup({";
+    if (get_option('wp_nameSkin')!="") echo "skin: '" . $directory . "/" . get_option('wp_nameSkin') . ".zip',";
+ 	echo $dimensions . "'flashplayer':'" .  $dirJwPlayer . "',
+ 	     file: '" . $arrayPlay[0]->urlPlay . "',
+         image: '" . $arrayPlay[0]->urlThumbs . "',
+ 	});</script>&nbsp;";
+	
+
+
+    
+    $output .= "<h3>" . $arrayPlay[0]->title . " (Preview)</h3>";
+    $output .= "<p>Duration: <b>" . $arrayPlay[0]->duration . "</b>";
+    if (count($arrayPlay[0]->categories)>0){
+      $output .= "<br/>Categories<br/>";
+      foreach ($arrayPlay[0]->categories as $key => $value) {
+        $valuescCatST = "<i>" . $value->categoryName . ":</i> ";
+        $output .= $valuescCatST;
+        foreach ($value->subCategories as $key => $value) {
+          $output .= $value->categoryName . ", ";
+        }
+        $output = substr($output, 0, -2); 
+        $output .= "<br/>";
+      }
+      $output .= "</p>";
+    }
+    echo $output . "</div>";
+ }   
+
+?>
