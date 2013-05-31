aedccfbeddf347ff25287fd8752c380e992b56c0
wimtv <wimtv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-31 15:18:03 +0000

    new version with producer.swf new
    git-svn-id: https://plugins.svn.wordpress.org/wimvideopro/tags/2.5.1@720878 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/embeddedLive.php
@@ -0,0 +1,11 @@
+<?php
+  include("../../../../wp-blog-header.php");
+
+echo "<div class='live_video'></div>";
+echo "<div class='live_list'></div>";
+
+wimtvpro_elencoLive("video", "0");
+wimtvpro_elencoLive("list", "0");
+
+
+?>
